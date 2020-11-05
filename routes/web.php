ef2e9470ed7fd07dd92864d118166709b04cc1ad
aguerram <agurram2013@gmail.com>
2020-11-05 16:31:55 +0100

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/enrg3', function () {
+        return view('dashboard');
+    })->name('enrg3');
+
+    Route::get('/enrg4', function () {
+        return view('dashboard');
+    })->name('enrg4');
+
+    Route::get('/note3', function () {
+        return view('dashboard');
+    })->name('note3');
+
+    Route::get('/note4', function () {
+        return view('dashboard');
+    })->name('note4');
+});
