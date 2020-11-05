f7d45e603b4b47ab6520ba415c4ea8981e092133
Oussidi Mohamed <osidi1998@gmail.com>
2020-11-05 19:28:39 +0100

    add mark-presence page

--- app/Http/Controllers/Enregistrement.php
+++ app/Http/Controllers/Enregistrement.php
@@ -10,46 +10,46 @@ class Enregistrement extends Controller
 {
     public function enrg4_submit(Request $request)
     {
-        $user = Etudiant4a::where('4a_matricule', '=', $request->user)->orWhere('4a_cin',"=", $request->user)->first();
+        $user = Etudiant4a::where('4a_matricule', '=', $request->user)->orWhere('4a_cin', "=", $request->user)->first();
         if ($user === null) {
-            return view("enrg4")->with(["message"=>"The user CIN or Matricule doesn't exist"]);
-        }else{
-            return view("mark-presence")->with(["user"=>$user]);
+            return view("enrg")->with(["message" => "The user CIN or Matricule doesn't exist"]);
+        } else {
+            return view("mark-presence")->with(["user" => $user]);
         }
     }
 
     public function enrg3_submit(Request $request)
     {
-        $user = Etudiant3a::where('3a_matricule', '=', $request->user)->orWhere('3a_cin',"=", $request->user)->first();
+        $user = Etudiant3a::where('3a_matricule', '=', $request->user)->orWhere('3a_cin', "=", $request->user)->first();
         if ($user === null) {
-            return view("enrg3")->with(["message"=>"The user CIN or Matricule doesn't exist"]);
-        }else{
-            return view("mark-presence")->with(["user"=>$user]);
+            return view("enrg")->with(["message" => "The user CIN or Matricule doesn't exist"]);
+        } else {
+            return view("mark-presence")->with(["user" => $user]);
         }
     }
 
-
-
-
-
-    
+    // for updating student presence
     public function mark_presence3(Request $request)
     {
-        $user = Etudiant4a::where('4a_matricule', '=', $request->user)->orWhere('4a_cin',"=", $request->user)->first();
+        $user = Etudiant3a::where('3a_matricule', '=', $request->user)->orWhere('3a_cin', "=", $request->user)->first();
         if ($user === null) {
-            return view("enrg4")->with(["message"=>"The user CIN or Matricule doesn't exist"]);
-        }else{
-            return view("mark-presence")->with(["user"=>$user]);
+            return view("mark-presence")->with(["message" => "The user CIN or Matricule doesn't exist", "error" => true]);
+        } else {
+            $user['3a_presence'] = 1;
+            $user->save();
+            return view("mark-presence")->with(["user" => $user, "message" => "The student has been marked as present", "error" => false]);
         }
     }
 
     public function mark_presence4(Request $request)
     {
-        $user = Etudiant3a::where('3a_matricule', '=', $request->user)->orWhere('3a_cin',"=", $request->user)->first();
+        $user = Etudiant4a::where('4a_matricule', '=', $request->user)->orWhere('4a_cin', "=", $request->user)->first();
         if ($user === null) {
-            return view("enrg3")->with(["message"=>"The user CIN or Matricule doesn't exist"]);
-        }else{
-            echo ($user);
+            return view("mark-presence")->with(["user" => $user, "message" => "The user CIN or Matricule doesn't exist", "error" => true]);
+        } else {
+            $user['4a_presence'] = 1;
+            $user->save();
+            return view("mark-presence")->with(["user" => $user, "message" => "The student has been marked as present", "error" => false]);
         }
     }
 
--- resources/views/mark-presence.blade.php
+++ resources/views/mark-presence.blade.php
@@ -11,21 +11,25 @@
             <form class="mt-8 p-5 w-full"
                 action="{{Request::is('enrg3') ? route('mark-presence3') : route('mark-presence4')}}" method="POST">
                 @if (isset($message))
-                <div class="bg-red-100 p-5 w-full rounded mb-4">
+                @php
+                $color= $error ? "red":"green";
+                @endphp
+                <div class="bg-{{$color}}-100 p-5 w-full rounded mb-4">
                     <div class="flex justify-between">
                         <div class="flex space-x-3">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                class="flex-none fill-current text-red-500 h-4 w-4">
+                                class="flex-none fill-current text-{{$color}}-500 h-4 w-4">
                                 <path
                                     d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z" />
                             </svg>
-                            <div class="flex-1 leading-tight text-sm text-red-700 font-medium">{{$message}}</div>
+                            <div class="flex-1 leading-tight text-sm text-{{$color}}-700 font-medium">{{$message}}</div>
                         </div>
                     </div>
                 </div>
                 @endif
                 @csrf
-                <input id="user" value="{{$user}}" name="user" type="hidden" required>
+                <input id="user" value="{{Request::is('enrg3') ? $user['3a_cin'] : $user['4a_cin']}}" name="user"
+                    type="hidden" required>
 
                 <div class="box flex">
                     <div class="w-1/3">
@@ -41,13 +45,16 @@
                             <div class="pt-3"><strong>Matricule</strong></div>
                         </div>
                         <div class="w-1/2">
-                            <div>{{Request::is('enrg3') ? $user->3a_nom : $user->3a_nom}}</div>
-                            <div class="pt-3">{{Request::is('enrg3') ? $user->3a_cin : $user->4a_cin}}</div>
-                            <div class="pt-3">{{Request::is('enrg3') ? $user->3a_cne : $user->4a_cne}}</div>
-                            <div class="pt-3">{{Request::is('enrg3') ? $user->3a_cin : $user->4a_cne}}</div>
-                            <div class="pt-3">{{Request::is('enrg3') ? $user->3a_cin : $user->4a_cne}}</div>
+                            <div>
+                                {{Request::is('enrg3') ? $user['3a_nom']." ".$user['3a_prenom'] : $user['4a_nom']." ".$user['4a_prenom']}}
+                            </div>
+                            <div class="pt-3">{{Request::is('enrg3') ? $user['3a_cin'] : $user['4a_cin']}}</div>
+                            <div class="pt-3">{{Request::is('enrg3') ? $user['3a_massar'] : $user['4a_massar']}}</div>
+                            <div class="pt-3">{{Request::is('enrg3') ? $user['3a_filiere'] : $user['4a_filiere']}}</div>
+                            <div class="pt-3">{{Request::is('enrg3') ? $user['3a_matricule'] : $user['4a_matricule']}}
+                            </div>
                         </div>
-                        
+
                     </div>
 
                 </div>
@@ -57,10 +64,10 @@
                         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
                         Marquer la présence
                     </button>
-                    <button id="annuler" type="submit"
+                    <a href="{{Request::is('enrg3') ? route('enrg3') : route('enrg4')}}"
                         class="mt-3 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-black bg-gray-200 hover:bg-gray-300 text-black-800 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
                         Annuler
-                    </button>
+                    </a>
                 </div>
             </form>
         </div>
