893056a5049f01b49ee6e316683427e31f4b8939
aguerram <agurram2013@gmail.com>
2021-08-06 19:41:39 +0100

    livewire add filiere

--- resources/views/livewire/filieres-list.blade.php
+++ resources/views/livewire/filieres-list.blade.php
@@ -2,8 +2,9 @@
     @php
         $disable = date('Y') === '2021' && $key === 'I';
         $bg = $disable ?'bg-red-200':'bg-gray-200';
+        $link = $disable?'#':"/notes/$year/?filiere=$key"
     @endphp
-    <a @if($disable) disabled @endif class="card" href="/notes/{{$year}}/?filiere={{$key}}">
+    <a @if($disable) disabled @endif class="card" href="{{$link}}">
     <span
         class="focus:bg-gray-400 inline-block {{$bg}} rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
         {{$fil}}
